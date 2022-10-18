e9139a4d157d442830a3ea767b3516c043b426d9
qoterwoter <marderics@gmail.com>
2022-10-18 22:08:58 +0300

    asd

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const tasks = document.querySelector('.tasks')
 const deleteList = document.querySelector('.deleted')
 const taskList = document.querySelector('.tasks')
+let counter = 1;
 
 function submit(taskValue) {
     let row = document.createElement('div')
@@ -11,34 +12,34 @@ function submit(taskValue) {
     let task = document.querySelector('.enterItem')
 
     let deleteButton = document.createElement('button')
-    deleteButton.innerHTML = 'Удалить'
+    deleteButton.innerHTML = 'Завершить'
     deleteButton.classList = 'btn btn-danger col-4'
     deleteButton.addEventListener("click", deleteTask)
 
+    title.innerHTML = `${counter}. `
     if (task.value != '') { // Написал чтобы можно было добавлять задачи вызовом функции
-        title.innerHTML = task.value
+        title.innerHTML += task.value
     } else {
-        title.innerHTML = taskValue
+        title.innerHTML += taskValue
     }
 
     row.appendChild(title)
     row.appendChild(deleteButton)
     task.value = '' // Очистка input
-
+    counter++;
     tasks.appendChild(row)
 }
 
 function deleteTask() {
     if (this.classList.contains('btn-success')) {
         this.classList = 'btn btn-danger col-4'
-        this.innerHTML = 'Удалить'
+        this.innerHTML = 'Завершить'
         taskList.appendChild(this.parentNode)
     } else {
         this.classList = 'btn btn-success col-4'
         this.innerHTML = 'Восстановить'
         deleteList.appendChild(this.parentNode)
     }
-    this.addEventListener("click", restoreTask)
 }
 
 
